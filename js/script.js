973dd1d52fe0ee9d2751007b3d4038e0e37c3646
christophe126 <90414556+christophe126@users.noreply.github.com>
2022-07-04 17:06:13 +0200

    menu hamburger

--- js/script.js
+++ js/script.js
@@ -3,9 +3,7 @@ const menu = document.getElementById('menu')
 
 btn.addEventListener('click', navTogglee)
 
-
 function navTogglee() {
-  console.log("hello")
   btn.classList.toggle('open')
   menu.classList.toggle('flex')
   menu.classList.toggle('hidden')
